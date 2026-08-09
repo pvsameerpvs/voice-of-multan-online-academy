bd6d1a41801bfb645f25a2954081ff7fd4daecce
pvsameerpvs <pvsameerpvs@gmail.com>
2026-08-09 06:20:50 +0400

    fx: ai sectio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -72,7 +72,7 @@ export default function Home() {
       <AILearningSection />
       <CertificateShowcase />
       <TestimonialsSection />
-      <CTABand />
+      {/* <CTABand /> */}
     </>
   );
 }
